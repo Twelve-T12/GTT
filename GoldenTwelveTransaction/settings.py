0505756455131b938231c9b1b02053c5cfcbbc2a
Twelve-T12 <babalekan14@gmail.com>
2025-05-04 16:18:00 -0700

    Update whitenoise

--- GoldenTwelveTransaction/settings.py
+++ GoldenTwelveTransaction/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.midddleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'GoldenTwelveTransaction.urls'
